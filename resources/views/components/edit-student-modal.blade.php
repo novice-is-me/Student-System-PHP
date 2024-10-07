29591f97d546ba80ba8616a248a43ec3bd3bc5c2
Regina Jalandoni <reginajalandoni13@gmail.com>
2024-10-07 21:59:32 +0800

    Add update form

--- resources/views/components/edit-student-modal.blade.php
+++ resources/views/components/edit-student-modal.blade.php
@@ -1,17 +1,31 @@
 <div
-    x-data="{ showStudent: false }"
-    x-show="showStudent"
-    x-on:open-edit-student.window="showStudent = true"
-    x-on:close-edit-student.window="showStudent = false"
-    x-on:keydown.escape.window="showStudent = false"
-    x-transition:enter="transition ease-out duration-300"
-    style="display: none"
-    class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50"
+x-data="{ showStudent: false }"
+x-show="showStudent"
+x-on:open-edit-student.window="showStudent = true"
+x-on:close-edit-student.window="showStudent = false"
+x-on:keydown.escape.window="showStudent = false"
+x-transition:enter="transition ease-out duration-300"
+style="display: none"
+class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50"
 >   
-    <div class="bg-white p-6 rounded-lg">
-        <p>{{ $user->first_name }}</p>
-        <form action="">
-            
-        </form>
-    </div>
+<div class="bg-white p-6 rounded-lg">
+    <form class="max-w-sm mx-auto">
+        <div class="mb-5">
+            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 ">First Name</label>
+            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            value="{{ $user->first_name }}"/>
+        </div>
+        <div class="mb-5">
+            <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 ">Last Name</label>
+            <input type="text" id="last_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            value="{{ $user->last_name }}" />
+        </div>
+        <div class="mb-5">
+            <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Email</label>
+            <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            value="{{ $user->email }}"/>
+        </div>
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    </form>
+</div>
 </div>
